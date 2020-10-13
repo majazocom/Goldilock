5e1ea88269ac02ad9f322c34cb887e182016d994
Maja Gedda <maja.gedda@zocom.se>
2020-10-13 13:57:00 +0200

    shorter js

--- script.js
+++ script.js
@@ -5,22 +5,28 @@ const chapOneOptTwoFb = '"This porridge is too cold" she said.';
 //Start frame
 document.getElementById('start-button').addEventListener('click', function() {
     //Toggle hide class
-    document.getElementById('chapter-one').classList.toggle('hide');
     document.getElementById('start-frame').classList.toggle('hide');
+    document.getElementById('chapter-one').classList.toggle('hide');
 });
 
+// Chapter one
+const chapterOneOptionOne = document.getElementById('chapter-one-option-one');
+const chapterOneFeedback = document.getElementById('chapter-one-feedback');
+const chapterOneOptionTwo = document.getElementById('chapter-one-option-two');
+const chapterOneOptionThree = document.getElementById('chapter-one-option-three');
+
 //When the first chapter is rendered and first option that is incorrect is selected
-document.getElementById('chapter-one-option-one').addEventListener('click', function() {
-    document.getElementById('chapter-one-feedback').innerHTML = chapOneOptOneFb;
+chapterOneOptionOne.addEventListener('click', function() {
+    chapterOneFeedback.innerHTML = chapOneOptOneFb;
 });
 
 //When the first chapter is rendered and second option that is incorrect is selected
-document.getElementById('chapter-one-option-two').addEventListener('click', function() {
-    document.getElementById('chapter-one-feedback').innerHTML = chapOneOptTwoFb;
+chapterOneOptionTwo.addEventListener('click', function() {
+    chapterOneFeedback.innerHTML = chapOneOptTwoFb;
 });
 
 //Correct option selected -> render chapter 2
-document.getElementById('chapter-one-option-three').addEventListener('click', function() {
-    document.getElementById('chapter-two').classList.toggle('hide');
+chapterOneOptionThree.addEventListener('click', function() {
     document.getElementById('chapter-one').classList.toggle('hide');
+    document.getElementById('chapter-two').classList.toggle('hide');
 });
