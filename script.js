155bd0603317a347c417710309a2de811905d46f
Maja Gedda <maja.gedda@zocom.se>
2020-10-13 14:26:15 +0200

    to variables

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 //Global constants
 const chapOneOptOneFb = '"This porridge is too hot!" she exclaimed.';
 const chapOneOptTwoFb = '"This porridge is too cold" she said.';
+const startButton = document.getElementById('start-button');
+const chapterOne = document.getElementById('chapter-one');
 
 //Start frame
-document.getElementById('start-button').addEventListener('click', function() {
+startButton.addEventListener('click', function() {
     //Toggle hide class
+    chapterOne.classList.toggle('hide');
     document.getElementById('start-frame').classList.toggle('hide');
-    document.getElementById('chapter-one').classList.toggle('hide');
 });
 
-// Chapter one
-const chapterOneOptionOne = document.getElementById('chapter-one-option-one');
-const chapterOneFeedback = document.getElementById('chapter-one-feedback');
-const chapterOneOptionTwo = document.getElementById('chapter-one-option-two');
-const chapterOneOptionThree = document.getElementById('chapter-one-option-three');
-
 //When the first chapter is rendered and first option that is incorrect is selected
-chapterOneOptionOne.addEventListener('click', function() {
-    chapterOneFeedback.innerHTML = chapOneOptOneFb;
+document.getElementById('chapter-one-option-one').addEventListener('click', function() {
+    document.getElementById('chapter-one-feedback').innerHTML = chapOneOptOneFb;
 });
 
 //When the first chapter is rendered and second option that is incorrect is selected
-chapterOneOptionTwo.addEventListener('click', function() {
-    chapterOneFeedback.innerHTML = chapOneOptTwoFb;
+document.getElementById('chapter-one-option-two').addEventListener('click', function() {
+    document.getElementById('chapter-one-feedback').innerHTML = chapOneOptTwoFb;
 });
 
 //Correct option selected -> render chapter 2
-chapterOneOptionThree.addEventListener('click', function() {
-    document.getElementById('chapter-one').classList.toggle('hide');
+document.getElementById('chapter-one-option-three').addEventListener('click', function() {
     document.getElementById('chapter-two').classList.toggle('hide');
+    document.getElementById('chapter-one').classList.toggle('hide');
 });
