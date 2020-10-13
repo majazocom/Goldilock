dd74cf46dbc704bd78433a09799ae646acbf6980
Maja Gedda <maja.gedda@zocom.se>
2020-10-13 10:51:15 +0200

    added some script

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+jsconst chapOneOptOneFb = '"This porridge is too hot!" she exclaimed.';
+
 document.getElementById('start-button').addEventListener('click', function() {
     document.getElementById('chapter-one').classList.toggle('hide');
     document.getElementById('start-frame').classList.toggle('hide');
+});
+
+document.getElementById('chapter-one-option-one').addEventListener('click', function() {
+    
+});
+
+document.getElementById('chapter-one-option-three').addEventListener('click', function() {
+    document.getElementById('chapter-two').classList.toggle('hide');
+    document.getElementById('chapter-one').classList.toggle('hide');
 });
