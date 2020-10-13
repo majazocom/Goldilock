6943a92ff8ccaac117cbec67c689192632af04d5
Maja Gedda <maja.gedda@zocom.se>
2020-10-13 10:26:05 +0200

    logic to start frame added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('chapter-one').classList.toggle('hide');
+    document.getElementById('start-frame').classList.toggle('hide');
+});
